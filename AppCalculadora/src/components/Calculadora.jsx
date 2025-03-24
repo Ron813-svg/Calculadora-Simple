bba36b32b3f72d254c75539a7ad15b37c106f7e5
Rony <ronyjavierramirezalvarado@gmail.com>
2025-03-24 09:47:34 -0600

    v1

---
+++ AppCalculadora/src/components/Calculadora.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import './Calculadora.css'
+
+const Calculadora = () => {
+    const [num1, setNum1] = useState('')
+    const [num2, setNum2] = useState('')
+    const [result, setResult] = useState(null)
+
+    const handleSum = () => {
+        const sum = parseFloat(num1) + parseFloat(num2)
+        setResult(sum)
+    };
+    const handleRes = () => {
+        const res = parseFloat(num1) - parseFloat(num2)
+        setResult(res)
+    };
+    const handleDiv = () => {
+        const res = parseFloat(num1) / parseFloat(num2)
+        setResult(res)
+    };
+    const handleMult = () => {
+        const res = parseFloat(num1) * parseFloat(num2)
+        setResult(res)
+    };
+
+
+    return (
+        <div className="calculadora">
+            <h1>Calculadora</h1>
+            <input
+                type="number"
+                placeholder="Número 1"
+                value={num1}
+                onChange={(e) => setNum1(e.target.value)}
+            />
+            <input
+                type="number"
+                placeholder="Número 2"
+                value={num2}
+                onChange={(e) => setNum2(e.target.value)}
+            />
+            <button onClick={handleSum}>Sumar</button>
+            <br/>
+            <button onClick={handleRes}>Restar</button>
+            <br/>
+            <button onClick={handleDiv}>Dividir</button>
+            <br/>
+            <button onClick={handleMult}>Multiplicar</button>
+            <br/>
+            {result !== null && <p>Resultado: {result}</p>}
+        </div>
+    );
+
+
+}
+export default Calculadora
